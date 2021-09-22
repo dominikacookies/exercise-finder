a91b51bd255d61145177441990fb87ede9c10edc
Dominika <domi_nika@hotmail.co.uk>
2021-09-22 20:32:38 +0100

    algined and styled components on hp

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router } from "react-router-dom";
 
+import "./Reset.css";
 import "./App.css";
 import Routes from "./Routes";
 
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,12 +1,14 @@
 import Button from "../../components/Button";
+import "./home.css";
+import background from "../../images/hp-bkg.png";
 
 const Home = () => {
   return (
-    <main>
-      <div>
+    <main style={{ backgroundImage: `url(${background})` }}>
+      <div className="home-content-container">
         <h1>Tomorrow begins today.</h1>
         <h2>Start your workout now.</h2>
-        <div>
+        <div className="home-buttons-container">
           <Button label="Female" mode="primary" size="large" />
           <Button label="Male" mode="primary" size="large" />
         </div>
