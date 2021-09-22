c8b80099c6adcb6bfe53465f6442a849184d566d
Dominika <domi_nika@hotmail.co.uk>
2021-09-22 20:11:22 +0100

    unstyled homepage components

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,3 +1,18 @@
-const Home = () => {};
+import Button from "../../components/Button";
+
+const Home = () => {
+  return (
+    <main>
+      <div>
+        <h1>Tomorrow begins today.</h1>
+        <h2>Start your workout now.</h2>
+        <div>
+          <Button label="Female" mode="primary" size="large" />
+          <Button label="Male" mode="primary" size="large" />
+        </div>
+      </div>
+    </main>
+  );
+};
 
 export default Home;
