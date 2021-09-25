57ba02183fd0be78658aeb314676a48ee24fe81a
Dominika <domi_nika@hotmail.co.uk>
2021-09-25 12:22:25 +0100

    redirects to exercises page on click

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,16 +1,28 @@
 import Button from "../../components/Button";
 import "./home.css";
 import background from "../../images/hp-bkg.png";
+import { useHistory } from "react-router-dom";
 
 const Home = () => {
+  let history = useHistory();
   return (
     <main style={{ backgroundImage: `url(${background})` }}>
       <div className="home-content-container">
         <h1>Tomorrow begins today.</h1>
         <h2>Start your workout now.</h2>
         <div className="home-buttons-container">
-          <Button label="Female" mode="primary" size="large" />
-          <Button label="Male" mode="primary" size="large" />
+          <Button
+            label="Female"
+            mode="primary"
+            size="large"
+            onClick={() => history.push("/exercises/female")}
+          />
+          <Button
+            label="Male"
+            mode="primary"
+            size="large"
+            onClick={() => history.push("/exercises/male")}
+          />
         </div>
       </div>
     </main>
