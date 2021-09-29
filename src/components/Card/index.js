b4abefa11cfc6836e1b847b88b165ba7a1915764
Dominika <domi_nika@hotmail.co.uk>
2021-09-29 17:18:50 +0100

    WIP template code for card component

---
+++ src/components/Card/index.js
@@ -0,0 +1,16 @@
+import "./Card.css";
+
+const Card = () => {
+  return (
+    <div>
+      <div className="card-container">
+        <div className="card-preview-info">
+          <h4> title </h4>
+          <p> MUSCLES GO HERE </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
