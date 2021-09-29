c7acf723325747f5358ff1d4a6ae47fdc98f8312
Dominika <domi_nika@hotmail.co.uk>
2021-09-29 17:19:31 +0100

    fetches exercises. WIP page layout

--- src/pages/Exercises/index.js
+++ src/pages/Exercises/index.js
@@ -1,9 +1,44 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+
+import fetchExercises from "../../utils/fetchExercises";
+
+import femaleBkg from "../../images/exercisesFemale.jpg";
+import maleBkg from "../../images/exercisesMale.jpg";
+import Card from "../../components/Card";
+
+import "./exercises.css";
 
 const Exercises = () => {
   const { type } = useParams();
-  console.log(type);
-  return <h1>{type}</h1>;
+  const [exercises, setExercises] = useState();
+
+  useEffect(() => {
+    const getExercises = async () => {
+      const data = await fetchExercises();
+      const fetchedExercises = data.exercises;
+      setExercises(fetchedExercises);
+
+      console.log(fetchedExercises);
+    };
+
+    getExercises();
+  }, []);
+  return (
+    <main style={{ backgroundImage: `url(${femaleBkg})` }}>
+      <h1> Exercises </h1>
+      <div>
+        {exercises ? (
+          <Card />
+        ) : (
+          <div>
+            <h3> Sorry, we couldn't find any exercises at this time.</h3>
+            <p> Please try again later </p>
+          </div>
+        )}
+      </div>
+    </main>
+  );
 };
 
 export default Exercises;
