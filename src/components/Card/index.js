7ad3bbb10201a7774a23c163d5e39c73a937f9e2
Dominika <domi_nika@hotmail.co.uk>
2021-10-02 21:29:47 +0100

    prettier fixes

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,18 +1,22 @@
 import "./Card.css";
 
-const Card = ({ imageUrl, name, muscles, id }) => {
+const Card = ({ imageUrl, name, muscles, id, exerciseInstructions }) => {
   return (
-    <div className="card-container">
-      <div
-        style={{ backgroundImage: "url(" + imageUrl + ")" }}
-        className="card-image"
-      >
-        <div className="card-preview-info">
-          <h3 className="text-limit-one-line"> {name} </h3>
-          <p className="exercise-muscle-text text-limit-one-line">{muscles}</p>
+    <>
+      <div className="card-container">
+        <div
+          style={{ backgroundImage: "url(" + imageUrl + ")" }}
+          className="card-image"
+        >
+          <div className="card-preview-info">
+            <h3 className="text-limit-one-line"> {name} </h3>
+            <p className="exercise-muscle-text text-limit-one-line">
+              {muscles}
+            </p>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
