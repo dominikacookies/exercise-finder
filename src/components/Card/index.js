9941a00a99ffd06753ae2ef67882d498a57863c6
Dominika <domi_nika@hotmail.co.uk>
2021-10-02 15:08:04 +0100

    limited text to one line and styles card

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -2,14 +2,14 @@ import "./Card.css";
 
 const Card = ({ imageUrl, name, muscles, id }) => {
   return (
-    <div
-      style={{ backgroundImage: "url(" + imageUrl + ")" }}
-      className="card-image"
-    >
-      <div className="card-container">
+    <div className="card-container">
+      <div
+        style={{ backgroundImage: "url(" + imageUrl + ")" }}
+        className="card-image"
+      >
         <div className="card-preview-info">
-          <h4> {name} </h4>
-          <p> {muscles} </p>
+          <h3 className="text-limit-one-line"> {name} </h3>
+          <p className="exercise-muscle-text text-limit-one-line">{muscles}</p>
         </div>
       </div>
     </div>
